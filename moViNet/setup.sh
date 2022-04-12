6e64d8a1314d4fbbd0e4f32ff78493afff09e471
Matthew Jackson <mnj98@vt.edu>
2022-04-12 15:28:54 -0400

    setup

---
+++ moViNet/setup.sh
@@ -0,0 +1,5 @@
+wget https://raw.githubusercontent.com/tensorflow/models/f8af2291cced43fc9f1d9b41ddbf772ae7b0d7d2/official/projects/movinet/files/kinetics_600_labels.txt -O labels.txt -q
+
+wget https://github.com/tensorflow/models/raw/f8af2291cced43fc9f1d9b41ddbf772ae7b0d7d2/official/projects/movinet/files/jumpingjack.gif -O jumpingjack.gif -q
+
+python3 -m pip install --user -r reqs.txt
---
+++ mobile-net/mob.py
@@ -0,0 +1,22 @@
+from tensorflow.keras.applications import MobileNetV3Large
+from tensorflow.keras.preprocessing import image
+from tensorflow.keras.applications.mobilenet import preprocess_input, decode_predictions
+import numpy as np
+import sys
+model = MobileNetV3Large(weights='imagenet')
+
+if len(sys.argv) < 2:
+    print("Please specify image")
+img_path = sys.argv[1]
+#img_path = 'elephant.jpg'
+img = image.load_img(img_path, target_size=(224, 224))
+x = image.img_to_array(img)
+x = np.expand_dims(x, axis=0)
+x = preprocess_input(x)
+
+while 1:
+    preds = model.predict(x)
+# decode the results into a list of tuples (class, description, probability)
+# (one such list for each sample in the batch)
+#print('Predicted:', decode_predictions(preds, top=3)[0])
+# Predicted: [(u'n02504013', u'Indian_elephant', 0.82658225), (u'n01871265', u'tusker', 0.1122357), (u'n02504458', u'African_elephant', 0.061040461)]
