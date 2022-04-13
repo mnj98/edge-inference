773cfe5192decc279c029cebe25552901eb4e908
Matthew Jackson <mnj98@vt.edu>
2022-04-13 12:22:07 -0400

    updated .yml files

--- ResNet/res.py
+++ ResNet/res.py
@@ -3,6 +3,7 @@ from tensorflow.keras.preprocessing import image
 from tensorflow.keras.applications.resnet50 import preprocess_input, decode_predictions
 import numpy as np
 import sys
+import time
 model = ResNet50(weights='imagenet')
 
 if len(sys.argv) < 2:
@@ -14,9 +15,11 @@ x = image.img_to_array(img)
 x = np.expand_dims(x, axis=0)
 x = preprocess_input(x)
 
-while 1:
+for i in range(5):
+    start = time.time()
     preds = model.predict(x)
+    print("--- %s seconds ---" % (time.time() - start))
 # decode the results into a list of tuples (class, description, probability)
 # (one such list for each sample in the batch)
-#print('Predicted:', decode_predictions(preds, top=3)[0])
+    print('Predicted:', decode_predictions(preds, top=3)[0])
 # Predicted: [(u'n02504013', u'Indian_elephant', 0.82658225), (u'n01871265', u'tusker', 0.1122357), (u'n02504458', u'African_elephant', 0.061040461)]
