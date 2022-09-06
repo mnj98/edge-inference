0ee0fdd17a2d5c530d81f5ac473a7d246fcd15f3
Matthew Jackson <mnj98@vt.edu>
2022-09-06 12:47:04 -0400

    inital batching

--- dest.py
+++ dest.py
@@ -94,8 +94,8 @@ while num_sent_frames < num_frames_to_send:
     #x = preprocess_input(batch)
     print("process time =", time.time() - proc_time) #print(type(x))
     preds = model.predict(x, verbose=1, use_multiprocessing=False, workers=1, max_queue_size=1, batch_size=BATCH_SIZE)
-    print(preds)
-    print('Predicted:', decode_predictions(preds, top=3)[0])
+    #print(preds)
+    #print('Predicted:', decode_predictions(preds, top=3)[0])
 
     #print("frame????? ", type(frame)," ", frame)
     #_, enc = cv2.imencode('.jpg', frame)
