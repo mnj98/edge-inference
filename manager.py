4a0cadc9994699aa0f7fe1afc8d74d3cb0324567
Matthew Jackson <mnj98@vt.edu>
2022-10-11 14:52:30 +0000

    cleaned up manager

--- manager.py
+++ manager.py
@@ -16,7 +16,6 @@ model = MobileNetV3Large(weights='imagenet')
 
 
 def qpush(i, req, res, events):
-    print('qpush')
     run(req, res, events)
 
 
@@ -33,14 +32,10 @@ with multiprocessing.Manager() as manager:
 
     for proc in procs:
         proc.start()
-    print(procs[0].pid)
+
     while True:
         r = requests.get()
-        print('image', r)
         image = r['image']
-        #print("waiting")
-        #sleep(1)
-
 
         frame = np.expand_dims(image, axis=0)
         frame = preprocess_input(frame)
@@ -52,21 +47,3 @@ with multiprocessing.Manager() as manager:
         r['done_event'].set()
         events.put(manager.Event())
 
-#    for proc in procs:
-#        proc.join()
-#    for j in range(3):
-#        print('qget')
-#        print(q.get())
-#
-#    for proc in procs:
-#        proc.join()
-
-
-
-
-#manager = SyncManager(address=("", 12345), authkey=b'1234')
-#manager.get_server().serve_forever()
-#data_queue = manager.Queue()
-
-#data_queue.put("Hello World")
-#print("done")
