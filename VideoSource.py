9fc16dc21629d4861684b952a5d22efcb7cf0453
Matthew Jackson <mnj98@vt.edu>
2022-10-13 14:54:51 -0400

    updated api

--- VideoSource.py
+++ VideoSource.py
@@ -4,6 +4,7 @@ import random
 import time
 import numpy as np
 from ast import literal_eval
+import threading
 
 class Source(object):
     def __init__(self):
@@ -23,12 +24,18 @@ class Source(object):
         return (self.current_image_id - 1, jpeg.tobytes())
 
 
+def request_inference(image, id, index, result_buffer, model = 'mobilenet', url='http://localhost:1234/infer'):
+    req = requests.post(url, files = {'image': image}, data = {'id': id, 'model': model})
+    top_result = literal_eval(req.content.decode())[0]
 
+    result_buffer[index] = top_result
 
 
 def main():
     images = Source()
     num_to_test = 1000
+    fps = 30
+    frame_delay = 1 / fps 
     truths_file = open('/home/pi/ImageNet/2012/2012_ground_truth_ids.txt', 'r')
     true_classes = np.ndarray(shape=(num_to_test,), dtype='int32')
     inf_classes = np.ndarray(shape=(num_to_test,), dtype='int32')
@@ -37,13 +44,26 @@ def main():
         true_classes[i] = int(truths_file.readline()[1:])
     print(true_classes)
     print(inf_classes)
-    x = 3 / 0 
+
+
+
     image_id, image = images.get_frame()
-    image_id = int(random.random() * 1000)
+    #image_id = int(random.random() * 1000)
+
+    start_time = time.time()
+
+    for i in range(num_to_test):
+        threading.Thread(target=request_inference, args=(image, image_id, i, inf_classes)).start()
+        time.sleep(frame_delay - ((time.time() - start_time) % frame_delay))
+
+
+    print(inf_classes)
+    '''
     start = time.time()
     req = requests.post('http://localhost:1234/infer', \
         files = {'image': image}, data = {'id': image_id, 'model': 'mobilenet'})#data={'model': 'mobilenet', 'image': images.get_frame()})
     print(req.content, time.time() - start)
+    '''
 
 
 if __name__ == '__main__':
