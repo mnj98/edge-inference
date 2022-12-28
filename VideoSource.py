ff916caeb77fd9a925469ac99e39ee7ac7b5cd47
Matthew Jackson <mnj98@vt.edu>
2022-12-28 21:21:42 +0000

    added config object & timeout counting

--- VideoSource.py
+++ VideoSource.py
@@ -1,9 +1,98 @@
-import cv2
+import cv2, copy, time
+from threading import Lock
 
 on_pi = True
 
 images_path = "/home/pi/ImageNet/2012/val/ILSVRC2012_val_%08d.JPEG" if on_pi else \
     "/Users/mnj98/ImageNet/ILSVRC2012_img_val/ILSVRC2012_val_%08d.JPEG"
+
+
+class Config(object):
+    def __init__(self, source_fps, offload_fps, mps=1, enabled=True, shape=(224,224)):
+        self.locks = {'fps': Lock(),\
+            'mps': Lock(),\
+            'enabled': Lock(),\
+            'proc': Lock(),\
+            'timeout': Lock(),\
+            'pps': Lock(),\
+            'tps': Lock()}
+        self.ofps = offload_fps
+        self.sfps = source_fps
+        self.mps = mps
+        self.enabled = enabled
+        self.shape = shape
+
+        self.proc_count = 0
+        self.timeout_count = 0
+
+        self.pps = []
+        self.tps = []
+    def measure_and_report_fps(self):
+        with self.locks['pps']:
+            with self.locks['proc']:
+                if len(self.pps) < 1:
+                    self.pps.append((self.proc_count, time.time()))
+                    return None
+                delta_p = self.proc_count - self.pps[-1][0]
+                t = time.time()
+                delta_t = t - self.pps[-1][1]
+                fps = delta_p / delta_t
+                self.pps.append((self.proc_count, t))
+                return fps
+    def measure_and_report_tps(self):
+        with self.locks['tps']:
+            with self.locks['timeout']:
+                if len(self.tps) < 1:
+                    self.tps.append((self.timeout_count, time.time()))
+                    return None
+                delta_timeout = self.timeout_count - self.tps[-1][0]
+                t = time.time()
+                delta_t = t - self.tps[-1][1]
+                tps = delta_timeout / delta_t
+                self.tps.append((self.timeout_count, t))
+                return tps
+
+    def get_proc_rates(self):
+        with self.locks['pps']:
+            return copy.deepcopy(self.pps)
+    def get_timeout_rates(self):
+         with self.locks['tps']:
+             return copy.deepcopy(self.tps)
+    def disable_offloading(self):
+        with self.locks['enabled']:
+            self.enabled = False
+    def enable_offloading(self):
+        with self.locks['enabled']:
+            self.enabled = True
+    def is_offloading_enabled(self):
+        with self.locks['enabled']:
+            return self.enabled
+    def get_measure_rate(self):
+        with self.locks['mps']:
+            return self.mps
+    def set_measure_rate(self, n = 1):
+        with self.locks['mps']:
+            self.mps = n
+    def set_offload_fps(self, n = None):
+        if n == None:
+            n = self.sfps
+        with self.locks['fps']:
+            self.ofps = n
+    def get_offload_fps(self):
+        with self.locks['fps']:
+            return self.ofps
+    def add_proc(self, n = 1):
+        with self.locks['proc']:
+            self.proc_count += n
+    def add_timeout(self, n = 1):
+        with self.locks['timeout']:
+            self.timeout_count += n
+    def get_procs(self):
+        with self.locks['proc']:
+            return self.proc_count
+    def get_timeouts(self):
+        with self.locks['timeout']:
+            return self.timeout_count
 
 class VideoSource(object):
     def __init__(self, shape):
--- pi_manager.py
+++ pi_manager.py
@@ -1,12 +1,13 @@
 from VideoSource import VideoSource as Source
 from VideoSource import inf_request as Req
 from VideoSource import inf_response as Res
-import multiprocessing, time, threading, requests
+from VideoSource import Config
+import multiprocessing, time, threading, requests, sys
 from pi_local_infer import infer_loop
 import numpy as np
 from ast import literal_eval
 
-def request_offload(request, q, timeout, url='http://localhost:1234/infer'):
+def request_offload(request, q, config, timeout, url='http://localhost:1234/infer'):
     t = time.time()
     try:
         req = requests.post(url, files = {\
@@ -18,6 +19,7 @@ def request_offload(request, q, timeout, url='http://localhost:1234/infer'):
     except Exception as E: #requests.exceptions.Timeout as T:
         lat = time.time() - t
         print('failed', E)
+        config.add_timeout()
         q.put(Res(request.id, None, False, lat, False))
 
 
@@ -32,43 +34,35 @@ def process_results(q, arr, num_to_test):
     for i in range(num_to_test):
         result = q.get()
         arr[result.id] = result
-        print('lat for', result.id, 'is', result.latency)
+        #print('lat for', result.id, 'is', result.latency)
 
-def measure(config, lock, done):
-    d = 1 / config['measure_rate']
+def measure(config, done):
+    d = 1 / config.get_measure_rate()
     st = time.time()
     while not done.is_set():
-        with lock:
-            config['processed_in_time'].append((time.time(), config['processed']))
-            if len(config['processed_in_time']) > 1:
-                prev = config['processed_in_time'][-2]
-                curr = config['processed_in_time'][-1]
-                print("FPS:", (curr[1] - prev[1]) / (curr[0] - prev[0]))
-            #if len(config['processed_in_time']) > 15:
-            #   config['enabled'] = False
+        print("FPS", config.measure_and_report_fps())
+        print("TPS", config.measure_and_report_tps())
         wait = d - ((time.time() - st) % d)
         time.sleep(wait)
-def get_processed(config, lock):
-    with lock:
-        return config['processed']
 
 def main():
-    num_to_test = 60
-    config_lock = threading.Lock()
-    offload_config = {'frame_delay': 1/31,\
-         'enabled': True,\
-         'processed': 0,\
-         'processed_in_time': [],\
-         'measure_rate': 3}
-    fps = 30
-    frame_delay = 1 / fps
-    shape = (224,224)
+    num_to_test = int(sys.argv[1])
+    #config_lock = threading.Lock()
+    #offload_config = {'frame_delay': 1/31,\
+    #     'enabled': True,\
+    #     'processed': 0,\
+    #     'processed_in_time': [],\
+    #     'measure_rate': 3}
+    #fps = 30
+    config = Config(source_fps=30, offload_fps=40, mps=3)
+    frame_delay = 1 / config.sfps
+    shape = config.shape
     results_arr = np.ndarray((num_to_test,), dtype=Res)
     image_queue = multiprocessing.Queue(1)
     req_queue = multiprocessing.Queue(1)
     res_queue = multiprocessing.Queue()
     cap_proc = multiprocessing.Process(target=capture_loop, \
-        args=(image_queue, num_to_test, shape, 1/fps))
+        args=(image_queue, num_to_test, shape, 1/config.sfps))
     pull_from_queue_event = multiprocessing.Event()
     infer_ready = multiprocessing.Event()
     local_infer_proc = multiprocessing.Process( \
@@ -78,7 +72,7 @@ def main():
     
 
     measure_done_event = threading.Event()
-    measure_thread = threading.Thread(target=measure, args=(offload_config,config_lock,\
+    measure_thread = threading.Thread(target=measure, args=(config,\
         measure_done_event))
     pull_from_queue_event.set()
     #warm up
@@ -102,23 +96,22 @@ def main():
     start_time = time.time()
     print('start time', start_time)
 
-    while get_processed(offload_config, config_lock) < num_to_test:
+    while config.get_procs() < num_to_test:
         t_since_last_offload = time.time() - last_offload
-        if offload_config['enabled']\
-            and t_since_last_offload - offload_config['frame_delay'] > -0.003: #offload
+        if config.is_offloading_enabled()\
+            and t_since_last_offload - (1/config.get_offload_fps()) > -0.003: #offload
             req = image_queue.get()
             last_offload = time.time()
-            with config_lock:
-                offload_config['processed'] += 1
+            config.add_proc()
             o_count += 1
-            thread = threading.Thread(target=request_offload, args=(req, res_queue, 0.5))
+            thread = threading.Thread(target=request_offload, args=(req, res_queue, config, 0.15))
             thread.start()
             offload_threads.append(thread)
 
         elif infer_ready.is_set() and \
-            (offload_config['frame_delay'] > frame_delay or not offload_config['enabled']):
-            with config_lock:
-                offload_config['processed'] += 1
+            (config.get_offload_fps() > frame_delay\
+            or not config.is_offloading_enabled()):
+            config.add_proc()
             pull_from_queue_event.set()
         wait = frame_delay - ((time.time() - start_time) % frame_delay)
         time.sleep(wait)
