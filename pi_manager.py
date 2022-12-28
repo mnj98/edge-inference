92c0dcf38df1e08195affb9323468c06cd305e8c
Matthew Jackson <mnj98@vt.edu>
2022-12-28 17:29:22 +0000

    added fps measurements

--- pi_manager.py
+++ pi_manager.py
@@ -30,16 +30,32 @@ def process_results(q, arr, num_to_test):
         result = q.get()
         arr[result.id] = result
 
-def change_offload(o):
-    time.sleep(10)
-    o['frame_delay'] = 1 / 35
-    o['enabled'] = False
-
-
+def measure(config, lock, done):
+    d = 1 / config['measure_rate']
+    st = time.time()
+    while not done.is_set():
+        with lock:
+            config['processed_in_time'].append((time.time(), config['processed']))
+            if len(config['processed_in_time']) > 1:
+                prev = config['processed_in_time'][-2]
+                curr = config['processed_in_time'][-1]
+                print("FPS:", (curr[1] - prev[1]) / (curr[0] - prev[0]))
+            if len(config['processed_in_time']) > 15:
+                config['enabled'] = False
+        wait = d - ((time.time() - st) % d)
+        time.sleep(wait)
+def get_processed(config, lock):
+    with lock:
+        return config['processed']
 
 def main():
     num_to_test = 500
-    offload_config = {'frame_delay': 1/25, 'enabled': True}
+    config_lock = threading.Lock()
+    offload_config = {'frame_delay': 1/10,\
+         'enabled': True,\
+         'processed': 0,\
+         'processed_in_time': [],\
+         'measure_rate': 3}
     fps = 30
     frame_delay = 1 / fps
     shape = (224,224)
@@ -57,8 +73,9 @@ def main():
     res_thread = threading.Thread(target=process_results, args=(res_queue, results_arr, num_to_test))
     
 
-
-    c = threading.Thread(target=change_offload, args=(offload_config,))
+    measure_done_event = threading.Event()
+    measure_thread = threading.Thread(target=measure, args=(offload_config,config_lock,\
+        measure_done_event))
     pull_from_queue_event.set()
     #warm up
     infer_ready.wait()
@@ -75,45 +92,49 @@ def main():
 
 
     o_count = 0
-    processed = 0
+    #processed = 0
     offload_threads = []
-    c.start()
+    measure_thread.start()
     start_time = time.time()
     print('start time', start_time)
 
-    while processed < num_to_test:
+    while get_processed(offload_config, config_lock) < num_to_test:
         t_since_last_offload = time.time() - last_offload
         if offload_config['enabled']\
             and t_since_last_offload - offload_config['frame_delay'] > -0.003: #offload
             req = image_queue.get()
             last_offload = time.time()
-            processed += 1
+            with config_lock:
+                offload_config['processed'] += 1
             o_count += 1
             thread = threading.Thread(target=request_offload, args=(req, res_queue, 0.5))
             thread.start()
             offload_threads.append(thread)
-            wait = frame_delay - ((time.time() - start_time) % frame_delay)
-            time.sleep(wait)
-            continue
-        if infer_ready.is_set() and \
+
+        elif infer_ready.is_set() and \
             (offload_config['frame_delay'] > frame_delay or not offload_config['enabled']):
-            processed += 1
+            with config_lock:
+                offload_config['processed'] += 1
             pull_from_queue_event.set()
         wait = frame_delay - ((time.time() - start_time) % frame_delay)
         time.sleep(wait)
+        #with config_lock:
+        #    print(offload_config['processed_in_time'])
         continue
 
     res_thread.join()
+    
     for t in offload_threads:
         t.join()
     total_time = time.time() - start_time
+    measure_done_event.set()
+    measure_thread.join()
     print("Total time:", total_time, "FPS =", num_to_test / total_time)
     print("Offload %:", o_count / num_to_test)
     local_infer_proc.kill()
     local_infer_proc.join()
     local_infer_proc.close()
     cap_proc.join()
-    c.join()
 
 
 
